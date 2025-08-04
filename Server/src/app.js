399920ffa001b1f3761d67e9f6f9f0696958ffe4
Admin <admin@Admins-MacBook-Air.local>
2025-08-04 23:15:21 +0530

    fix

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-  origin: "https://ciaan-assignment-kapil.netlify.app",
+  origin: 'http://localhost:5173', 
   credentials: true
 }));
 
